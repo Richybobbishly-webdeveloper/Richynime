44e7c85d7464ac9a93bf8d03d5dc443c53553920
Richybobbishly2002 <richybobbishly@gmail.com>
2020-08-07 01:54:54 +0100

    Adding Includes Files

---
+++ includes/anime_list.php
@@ -0,0 +1,366 @@
+<link href="../css/styles.css" rel="stylesheet" />
+  
+   <header class="masthead2">
+    <div class="container">
+   
+    </div>
+</header>
+     
+   <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+    <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="../index.php"><img src="../assets/img/logo/richynime.jpg" alt="" /></a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                Menu
+                    <i class="fas fa-bars ml-1"></i>
+            </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav text-uppercase ml-auto">
+            <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#services">Services</a></li>
+            <li class="nav-item"><a class="nav-link js-scroll-trigger" href="../includes/anime_list.php">Anime List</a></li>
+            <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
+            <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#team">Team</a></li>
+            <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#contact">Contact</a></li>
+        </ul>
+    </div>
+    </div>
+</nav>
+  
+   <!-- Portfolio Grid-->
+    <section class="page-section bg-light" id="portfolio">
+        <div class="container">
+        <div class="text-center">
+            <h2 class="section-heading text-uppercase">
+                Anime List
+            </h2>
+            <h3 class="section-subheading text-muted">
+               Check Out And Download Any Anime Of Your Choice.
+            </h3>
+        </div>
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-6">
+        <div class="">
+        <div class="portfolio-item">
+            <a class="portfolio-link" data-toggle="modal" href="../animes_pages/re_zero_starting_life_in_another_world/re_zero_page_1.php">
+        <div class="portfolio-hover">
+        <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+        </div>
+            <img class="img-fluid" src="../assets/img/portfolio/01-2.jpg" alt="" />
+            </a>
+        <div class="portfolio-caption">
+        <div class="portfolio-caption-heading">
+            Threads
+        </div>
+        <div class="portfolio-caption-subheading text-muted">
+            Illustration
+        </div>
+        </div>
+        </div>
+        </div>
+        <div class="">
+        <div class="portfolio-item">
+            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal2">
+        <div class="portfolio-hover">
+        <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+        </div>
+            <img class="img-fluid" src="../assets/img/portfolio/02-2.jpg" alt="" />
+            </a>
+        <div class="portfolio-caption">
+        <div class="portfolio-caption-heading">
+            Explore
+        </div>
+        <div class="portfolio-caption-subheading text-muted">
+            Graphic Design
+        </div>
+        </div>
+        </div>
+        </div>
+        <div class="">
+        <div class="portfolio-item">
+            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal3">
+        <div class="portfolio-hover">
+        <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+        </div>
+            <img class="img-fluid" src="../assets/img/portfolio/03-2.jpg" alt="" />
+            </a>
+        <div class="portfolio-caption">
+        <div class="portfolio-caption-heading">
+            Finish
+        </div>
+        <div class="portfolio-caption-subheading text-muted">
+            Identity
+        </div>
+        </div>
+        </div>
+        </div>
+        <div class="">
+        <div class="portfolio-item">
+            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal4">
+        <div class="portfolio-hover">
+        <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+        </div>
+            <img class="img-fluid" src="../assets/img/portfolio/04-2.jpg" alt="" />
+            </a>
+        <div class="portfolio-caption">
+        <div class="portfolio-caption-heading">
+            Lines
+        </div>
+        <div class="portfolio-caption-subheading text-muted">
+            Branding
+        </div>
+        </div>
+        </div>
+        </div>
+        <div class="">
+        <div class="portfolio-item">
+            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal5">
+        <div class="portfolio-hover">
+        <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+        </div>
+            <img class="img-fluid" src="../assets/img/portfolio/05-2.jpg" alt="" />
+            </a>
+        <div class="portfolio-caption">
+        <div class="portfolio-caption-heading">
+            Southwest
+        </div>
+        <div class="portfolio-caption-subheading text-muted">
+            Website Design
+        </div>
+        </div>
+        </div>
+        </div>
+        <div class="">
+        <div class="portfolio-item">
+            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal6">
+        <div class="portfolio-hover">
+        <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+        </div>
+            <img class="img-fluid" src="../assets/img/portfolio/06-2.jpg" alt="" />
+            </a>
+        <div class="portfolio-caption">
+        <div class="portfolio-caption-heading">
+            Window
+        </div>
+        <div class="portfolio-caption-subheading text-muted">
+            Photography
+        </div>
+        </div>
+        </div>
+        </div>
+        <div class="">
+        <div class="portfolio-item">
+            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
+        <div class="portfolio-hover">
+        <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+        </div>
+            <img class="img-fluid" src="../assets/img/portfolio/07-2.jpg" alt="" />
+            </a>
+        <div class="portfolio-caption">
+        <div class="portfolio-caption-heading">
+            Threads
+        </div>
+        <div class="portfolio-caption-subheading text-muted">
+            Illustration
+        </div>
+        </div>
+        </div>
+        </div>
+        <div class="">
+        <div class="portfolio-item">
+            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal2">
+        <div class="portfolio-hover">
+        <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+        </div>
+            <img class="img-fluid" src="../assets/img/portfolio/08-2.jpg" alt="" />
+            </a>
+        <div class="portfolio-caption">
+        <div class="portfolio-caption-heading">
+            Explore
+        </div>
+        <div class="portfolio-caption-subheading text-muted">
+            Graphic Design
+        </div>
+        </div>
+        </div>
+        </div>
+        <div class="">
+        <div class="portfolio-item">
+            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal3">
+        <div class="portfolio-hover">
+        <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+        </div>
+            <img class="img-fluid" src="../assets/img/portfolio/09-2.jpg" alt="" />
+            </a>
+        <div class="portfolio-caption">
+        <div class="portfolio-caption-heading">
+            Finish
+        </div>
+        <div class="portfolio-caption-subheading text-muted">
+            Identity
+        </div>
+        </div>
+        </div>
+        </div>
+        <div class="">
+        <div class="portfolio-item">
+            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal4">
+        <div class="portfolio-hover">
+        <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+        </div>
+            <img class="img-fluid" src="../assets/img/portfolio/10-2.jpg" alt="" />
+            </a>
+        <div class="portfolio-caption">
+        <div class="portfolio-caption-heading">
+            Lines
+        </div>
+        <div class="portfolio-caption-subheading text-muted">
+            Branding
+        </div>
+        </div>
+        </div>
+        </div>
+        <div class="">
+        <div class="portfolio-item">
+            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal5">
+        <div class="portfolio-hover">
+        <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+        </div>
+            <img class="img-fluid" src="../assets/img/portfolio/11-2.jpg" alt="" />
+            </a>
+        <div class="portfolio-caption">
+        <div class="portfolio-caption-heading">
+            Southwest
+        </div>
+        <div class="portfolio-caption-subheading text-muted">
+            Website Design
+        </div>
+        </div>
+        </div>
+        </div>
+        <div class="">
+        <div class="portfolio-item">
+            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal6">
+        <div class="portfolio-hover">
+        <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+        </div>
+            <img class="img-fluid" src="../assets/img/portfolio/12-2.png" alt="" />
+            </a>
+        <div class="portfolio-caption">
+        <div class="portfolio-caption-heading">
+            Window
+        </div>
+        <div class="portfolio-caption-subheading text-muted">
+            Photography
+        </div>
+        </div>
+        </div>
+        </div>
+        <div class="">
+        <div class="portfolio-item">
+            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
+        <div class="portfolio-hover">
+        <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+        </div>
+            <img class="img-fluid" src="../assets/img/portfolio/13-2.jpg" alt="" />
+            </a>
+        <div class="portfolio-caption">
+        <div class="portfolio-caption-heading">
+            Threads
+        </div>
+        <div class="portfolio-caption-subheading text-muted">
+            Illustration
+        </div>
+        </div>
+        </div>
+        </div>
+        <div class="">
+        <div class="portfolio-item">
+            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal2">
+        <div class="portfolio-hover">
+        <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+        </div>
+            <img class="img-fluid" src="../assets/img/portfolio/14-2.jpg" alt="" />
+            </a>
+        <div class="portfolio-caption">
+        <div class="portfolio-caption-heading">
+            Explore
+        </div>
+        <div class="portfolio-caption-subheading text-muted">
+            Graphic Design
+        </div>
+        </div>
+        </div>
+        </div>
+        <div class="">
+        <div class="portfolio-item">
+            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal3">
+        <div class="portfolio-hover">
+        <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+        </div>
+            <img class="img-fluid" src="../assets/img/portfolio/15-2.jpg" alt="" />
+            </a>
+        <div class="portfolio-caption">
+        <div class="portfolio-caption-heading">
+            Finish
+        </div>
+        <div class="portfolio-caption-subheading text-muted">
+            Identity
+        </div>
+        </div>
+        </div>
+        </div>
+        <div class="">
+        <div class="portfolio-item">
+            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal4">
+        <div class="portfolio-hover">
+        <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+        </div>
+            <img class="img-fluid" src="../assets/img/portfolio/16-2.jpg" alt="" />
+            </a>
+        <div class="portfolio-caption">
+        <div class="portfolio-caption-heading">
+            Lines
+        </div>
+        <div class="portfolio-caption-subheading text-muted">
+            Branding
+        </div>
+        </div>
+        </div>
+        </div>
+        <div class="">
+        <div class="portfolio-item">
+            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal5">
+        <div class="portfolio-hover">
+        <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+        </div>
+            <img class="img-fluid" src="../assets/img/portfolio/17-2.png" alt="" />
+            </a>
+        <div class="portfolio-caption">
+        <div class="portfolio-caption-heading">
+            Southwest
+        </div>
+        <div class="portfolio-caption-subheading text-muted">
+            Website Design
+        </div>
+        </div>
+        </div>
+        </div>
+        <div class="">
+        <div class="portfolio-item">
+            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal6">
+        <div class="portfolio-hover">
+        <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+        </div>
+            <img class="img-fluid" src="../assets/img/portfolio/18-2.jpg" alt="" />
+            </a>
+        <div class="portfolio-caption">
+        <div class="portfolio-caption-heading">
+            Window
+        </div>
+        <div class="portfolio-caption-subheading text-muted">
+            Photography
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+    </section>
